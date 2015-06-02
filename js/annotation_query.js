d12c8d49dc5227ddb9d5bac4ed8aad98aabe31dc
Luminarys <kizunanohikari@gmail.com>
2015-06-02 02:30:18 -0500

    Tweaks and preparation for popups

--- js/annotation_query.js
+++ js/annotation_query.js
@@ -14,6 +14,59 @@ $.fn.dataTable.ext.search.push(
     }
 );
 
+function insertData(gene, species) {
+	$('#qTable').empty()
+	$('#info').empty()
+	$.get("php/annotation_query.php?gene=" + gene + "&spec=" + species, function(data) {
+		$('#info')
+		.append(data)
+		.ready(function(){
+			$("#annotationForm").hide();
+			$("#goBack").removeAttr('style').show();
+			$("#goBack").css("height","136px");	
+    			var mtable = $('#metricQueryTable').DataTable();
+		});
+	$.get("php/expression_query.php?g0=" + gene + "&spec=" + species, function(data) {
+		$('#qTable').append("<p>Gene Plot:</p>");
+		texts = [gene];
+		console.log(data);
+		if (!isJson(data)){
+			alert(data);
+			return 1;
+		}
+		var info = JSON.parse(data);
+		linePlot(info, texts);
+	});
+	});
+
+	$.get("php/gene_query.php?g0=" + gene + "&spec=" + species, function(data) {
+		$('#qTable').append("<p>Network Query Table:</p>");
+		$('#qTable')
+		.append(data)
+		.ready(function(){
+    			table = $('#basicQueryTable').DataTable();
+				$('#lower-rect').removeAttr('style');
+		});
+	});
+	table.draw();
+	/*
+	$.get("php/ortho_query", function(data) {
+		$('#qTable').empty()
+		.append(data)
+		.ready(function(){
+			if($('#orthoQueryTable tr').length > 9){
+				$('#lower-rect').removeAttr('style');
+			}else{
+				$("#goBack").css("height","136px");	
+			}
+    			table2 = $('#orthoQueryTable').DataTable();
+		});
+	});
+	table2.draw();
+	/**/
+
+}
+
 function getQueryVar(variable){
        var query = window.location.search.substring(1);
        var vars = query.split("&");
@@ -24,59 +77,19 @@ function getQueryVar(variable){
        return(false);
 }
 
+var table = $('#basicQueryTable').DataTable();
+
 $(document).ready(function() {
 
-    	var table = $('#basicQueryTable').DataTable();
-	//What to do when t
+    	table = $('#basicQueryTable').DataTable();
+	//What to do when we do an AJAX query to load a popup
 	if (getQueryVar("link")){
-		//Figure this out later
-		req = "php/gene_query.php?";
-       		var query = window.location.search.substring(1);
-       		var vars = query.split("&");
-		var first = true;
-		var cont = 0
-       		for (var i = 0;i < vars.length;i++) {
-               		var pair = vars[i].split("=");
-			if(i != 0){
-				req+="&";
-			}
-			if(pair[0].charAt(0) == "g"){
-				if(i != 0){
-					req+="&";
-				}
-				req+=("g" + cont + "=" + pair[1]);	
-				cont++;
-				if (first){
-					$('#multiGeneInputArea').val($('#multiGeneInputArea').val() + pair[1])
-					first = false;
-				}else{
-					$('#multiGeneInputArea').val($('#multiGeneInputArea').val() + '\n' + pair[1])
-				}
-			}else if(pair[0] == "spec"){
-				if(i != 0){
-					req+="&";
-				}
-				req+=("spec="+pair[1]);
-				$(".speciesSelect").val(pair[1]);
-			}
-       		}
-		req+=("&type=" + "OR");
-		console.log(req);
-		$.get(req, function(data) {
-			$('#qTable').empty()
-			.html(data)
-			.ready(function(){
-				$("#multiGeneForm").hide();
-				$("#goBack").show();
-				if($('#basicQueryTable tr').length > 9){
-					$('#lower-rect').removeAttr('style');
-				}else{
-					$("#goBack").css("height","136px");	
-				}
-    				table = $('#basicQueryTable').DataTable();
-			});
-		});
-		table.draw();
+		var gene = getQueryVar("gene");
+		var species = getQueryVar("spec");
+		$('.Site').empty();
+		$('.Site').append("<div id='info'></div>");
+		$('.Site').append("<div id='qTable'></div>");
+		insertData(gene, species);
 	}
 
 	$('*').keyup( function() {
@@ -89,57 +102,7 @@ $(document).ready(function() {
 		e.preventDefault();
 		var gene = $("#annotationInput").val();
 		var species = $(".speciesSelect").val();
-		$.get("php/annotation_query.php?gene=" + gene + "&spec=" + species, function(data) {
-			$('#qTable').empty()
-			.append(data)
-			.ready(function(){
-				$("#annotationForm").hide();
-				$("#goBack").removeAttr('style').show();
-				$("#goBack").css("height","136px");	
-    				var mtable = $('#metricQueryTable').DataTable();
-			});
-		$.get("php/gene_query.php?g0=" + gene + "&spec=" + species, function(data) {
-			$('#qTable').append("<p>Network Query Table:</p>");
-			$('#qTable')
-			.append(data)
-			.ready(function(){
-    				table = $('#basicQueryTable').DataTable();
-				if($('#basicQueryTable tr').length > 9){
-					$('#lower-rect').removeAttr('style');
-				}else{
-					$("#goBack").css("height","136px");	
-				}
-			});
-		});
-		table.draw();
-		$.get("php/expression_query.php?g0=" + gene + "&spec=" + species, function(data) {
-			$('#qTable').append("<p>Gene Plot:</p>");
-			texts = [gene];
-			console.log(data);
-			if (!isJson(data)){
-				alert(data);
-				return 1;
-			}
-			var info = JSON.parse(data);
-			linePlot(info, texts);
-		});
-		});
-
-		/*
-		$.get("php/ortho_query", function(data) {
-			$('#qTable').empty()
-			.append(data)
-			.ready(function(){
-				if($('#orthoQueryTable tr').length > 9){
-					$('#lower-rect').removeAttr('style');
-				}else{
-					$("#goBack").css("height","136px");	
-				}
-    				table2 = $('#orthoQueryTable').DataTable();
-			});
-		});
-		table2.draw();
-		/**/
+		insertData(gene, species);
 	});
 
 });
--- php/gene_query.php
+++ php/gene_query.php
@@ -6,8 +6,12 @@
 <!-- jQuery UI CSS -->
 <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"> -->
 
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.js"></script>
+
 <!-- DataTables CSS -->
 <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
+
   
 <!-- jQuery -->
 <script type="text/javascript" charset="utf8" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
@@ -15,8 +19,6 @@
 <!-- jQuery UI -->  
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
-<!-- DataTables -->
-<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.js"></script>
 
 </head>
 <body>
