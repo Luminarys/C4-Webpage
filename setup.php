c208bcf6f95cd727532f00a201e97eb6dee2b7f7
Luminarys <kizunanohikari@gmail.com>
2015-06-12 16:31:32 -0500

    Added in setup.php for creating configuration

--- setup.php
+++ setup.php
@@ -1,3 +1,4 @@
+<html>
 <?php
 function write_ini_file($assoc_arr, $path, $has_sections=FALSE) { 
     $content = ""; 
@@ -9,11 +10,11 @@ function write_ini_file($assoc_arr, $path, $has_sections=FALSE) {
                 { 
                     for($i=0;$i<count($elem2);$i++) 
                     { 
-                        $content .= $key2."[] = \"".$elem2[$i]."\"\n"; 
+                        $content .= $key2."[] = ".$elem2[$i]."\n"; 
                     } 
                 } 
                 else if($elem2=="") $content .= $key2." = \n"; 
-                else $content .= $key2." = \"".$elem2."\"\n"; 
+                else $content .= $key2." = ".$elem2."\n"; 
             } 
         } 
     } 
@@ -27,7 +28,7 @@ function write_ini_file($assoc_arr, $path, $has_sections=FALSE) {
                 } 
             } 
             else if($elem=="") $content .= $key." = \n"; 
-            else $content .= $key." = \"".$elem."\"\n"; 
+            else $content .= $key." = ".$elem."\n"; 
         } 
     } 
 
@@ -50,13 +51,14 @@ function test_input($data) {
 }
 
 // define variables and set to empty values
-$name = $copyright = $server = $user = $password = $maindb = $orthodb = "";
+$name = $github = $copyright = $server = $user = $password = $maindb = $orthodb = "";
 $gene_set = $module = $expression = $expression_prof = $ortholog = $annotation = false;
 
 //Process data and write settings file for a POSt request
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	$name = test_input($_POST["name"]);
+	$github = test_input($_POST["github"]);
 	$copyright = test_input($_POST["copyright"]);
 	$gene_set = test_input($_POST["gene_set"]);
 	$module = test_input($_POST["module"]);
@@ -72,30 +74,77 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	$result = array(
 	"Header" => array(
-   		$name
+   		"name" => $name,
+		"github" => $github
 	),
 	"Footer" => array(
-   		$copyright
+   		"copyright" => $copyright
 	),
 	"Queries" => array(
-  		 $gene_set
-  		 $module,
-  		 $expression,
-  		 $expression_prof,
-  		 $ortholog,
-  		 $annotation
+  		 "gene_set" => $gene_set,
+  		 "module" => $module,
+  		 "expression" => $expression,
+  		 "expression_prof" => $expression_prof,
+  		 "ortholog" => $ortholog,
+  		 "annotation" => $annotation
 	),
 	"DB" => array(
-		$server,
-		$user,
-		$password,
-		$maindb,
-		$orthodb
+		"server" => $server,
+		"user" => $user,
+		"password" => $password,
+		"maindb" => $maindb,
+		"orthodb" => $orthodb
 	)
 	);
-	write_ini_file($result, "./test.ini", true);	
-	echo ""
+	if(write_ini_file($result, "./settings.ini", true)){
+		echo "Settings saved, please open the website in a new tab to test it, and restrict access to this file and settings.ini";
+	}else{
+		echo "Warning, could not wrinte to the configuration file";
+	}
 }
-
-
 ?>
+	<h2>PHP Form Validation Example</h2>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		Site Name(used for the header title): <input type="text" name="name">
+		<br><br>
+		Github URL: <input type="text" name="github">
+		<br><br>
+		Copyright Holder: <input type="text" name="copyright">
+		<br><br>
+		 Gene Set Query:
+   		<input type="radio" name="gene_set" value="true">Enabled
+   		<input type="radio" name="gene_set" value="false">Disabled
+		<br><br>
+		 Module Query:
+   		<input type="radio" name="module" value="true">Enabled
+   		<input type="radio" name="module" value="false">Disabled
+		<br><br>
+		 Expression Query:
+   		<input type="radio" name="expression" value="true">Enabled
+   		<input type="radio" name="expression" value="false">Disabled
+		<br><br>
+		 Expression Profile Query:
+   		<input type="radio" name="expression_prof" value="true">Enabled
+   		<input type="radio" name="expression_prof" value="false">Disabled
+		<br><br>
+		 Ortholog Query:
+   		<input type="radio" name="ortholog" value="true">Enabled
+   		<input type="radio" name="ortholog" value="false">Disabled
+		<br><br>
+		 Annotation Query:
+   		<input type="radio" name="annotation" value="true">Enabled
+   		<input type="radio" name="annotation" value="false">Disabled
+		<br><br>
+		MySQL Server Address: <input type="text" name="server">
+		<br><br>
+		MySQL Server User: <input type="text" name="user">
+		<br><br>
+		MySQL Server Password: <input type="password" name="password">
+		<br><br>
+		MySQL Server Main Database Name: <input type="text" name="maindb">
+		<br><br>
+		MySQL Server Orthology Database Name: <input type="text" name="orthodb">
+		<br><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+</html>
