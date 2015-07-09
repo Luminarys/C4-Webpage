e6ab1f357db068e8efb1c700b9c0078ac41472d9
Luminarys <eumen@bensonhillbio.com>
2015-07-09 12:18:48 -0500

    Minor fixes to plasticity query

--- load_compare_options.php
+++ load_compare_options.php
@@ -14,12 +14,12 @@
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	//Build valid species and genes from MapReference table
-	$query = $db->prepare("SELECT pfx_compare, MapReference.display_name FROM MapCompare LEFT JOIN MapReference ON MapCompare.pfx_compare = MapReference.prefix WHERE pfx_control = ?");
+	$query = $db->prepare("SELECT MapCompare.prefix, pfx_compare, MapReference.display_name FROM MapCompare LEFT JOIN MapReference ON MapCompare.pfx_compare = MapReference.prefix WHERE pfx_control = ?");
 	if($query->execute(array($_GET["spec"]))){
 		echo "<select id='compareSelect'>";
 		$result = $query->fetchAll();
 		foreach ($result as $spec){
-			echo "<option value=" . $spec["pfx_compare"] . ">" . $spec["display_name"] . "</option>";
+			echo "<option prefix='" . $spec["prefix"] . "' value=" . $spec["pfx_compare"] . ">" . $spec["display_name"] . "</option>";
 		}
 		echo "</select>";
 	}else{
